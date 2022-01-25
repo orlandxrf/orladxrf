0fadec65e9ce0723bb9253a9bfa062dda21975dd
Orlando Ramos <orlandxrf@gmail.com>
2022-01-25 13:19:59 -0600

    updates

--- js/style.js
+++ js/style.js
@@ -10,4 +10,19 @@ $(function(){
 		}
 	});
 
+	$('.pdfBtn').on('click', function(){
+		$(this).blur();
+	});
+
+	$('.showCite').on('click', function(){
+		var id_cite = $(this).attr('data-cite')
+		if ($('#paper'+id_cite+'').css('display') == 'none') {
+			$('#paper'+id_cite+'').slideUp('slow', function(){
+				$(this).slideDown('slow');
+			});
+		} else if ($('#paper'+id_cite+'').css('display') == 'block') {
+			$('#paper'+id_cite+'').slideUp('slow');
+		}
+	});
+
 });
